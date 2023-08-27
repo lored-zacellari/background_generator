d3dc551fb1405a3714bde45ed211ee48524a6e14
unknown <lorizacellari@gmail.com>
2023-08-27 18:52:03 +0200

    adding starting project

---
+++ script.js
@@ -0,0 +1,17 @@
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var h3 = document.querySelector("h3");
+var body = document.getElementById("gradient");
+var resetColor = document.querySelector("button");
+
+function setColor(){
+	body.style.background="linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+	h3.textContent = body.style.background + ";";
+}
+color1.addEventListener("input", setColor);
+
+color2.addEventListener("input", setColor);
+
+resetColor.addEventListener("click", function(){
+body.style.background = "linear-gradient(to right, red , yellow)";
+});
